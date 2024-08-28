352f9495ed0bdf7278b97e4d5f53fb091e77e804
Kingcedru <byishimocedrick@gmail.com>
2024-08-28 19:28:23 +0200

    changed styles

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -12,6 +12,8 @@ import { decord } from 'src/app/utils/decord';
 export class ArticleComponent implements OnInit {
   articleTitle: string | null = null;
   article?: Articles;
+  noteMessage: string =
+    'This page is supposed to display the full text of the article, however, the news API we are using for this exercise project does not deliver the full text of the article. That’s why you are only seeing an article summary.';
 
   constructor(
     private route: ActivatedRoute,
@@ -19,25 +21,29 @@ export class ArticleComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.pipe(
-      switchMap((params) => {
-        const title = params.get('id');
-        if (title) {
-          return this.newsService.getNews().pipe(
-            map((response) =>
-              response.articles.find((article:any) => article.title === title)
-            ),
-            catchError((error) => {
-              console.error(error);
-              return of(undefined); // Return empty result on error
-            })
-          );
-        } else {
-          return of(undefined); // Return empty result if no title
-        }
-      })
-    ).subscribe(article => {
-      this.article = article || undefined;
-    });
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const title = params.get('id');
+          if (title) {
+            return this.newsService.getNews().pipe(
+              map((response) =>
+                response.articles.find(
+                  (article: any) => article.title === title
+                )
+              ),
+              catchError((error) => {
+                console.error(error);
+                return of(undefined); // Return empty result on error
+              })
+            );
+          } else {
+            return of(undefined); // Return empty result if no title
+          }
+        })
+      )
+      .subscribe((article) => {
+        this.article = article || undefined;
+      });
   }
 }
