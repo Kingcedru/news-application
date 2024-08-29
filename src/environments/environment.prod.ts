72427c31ac693017a838945b26476dbb588e5146
Kingcedru <byishimocedrick@gmail.com>
2024-08-29 18:25:04 +0200

    variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl:
+    'https://news-proxy.netlify.app/?apiKey=ce0791cf7cb34350929466338dfff3a9',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl:
+    'https://newsapi.org/v2/everything?q=tesla&from=2024-07-29&sortBy=publishedAt&apiKey=ce0791cf7cb34350929466338dfff3a9',
+};
