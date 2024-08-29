718a886b9bddcee5d1d9b77f8c077e9d5c746b11
Kingcedru <byishimocedrick@gmail.com>
2024-08-29 17:21:26 +0200

    responsiveness and searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'News-Application';
+
+  goToPreviousPage(){
+    window.history.back()
+  }
 }
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Articles, newsDto } from 'src/app/Dto/news';
+import { Articles } from 'src/app/Dto/news';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
@@ -11,6 +11,7 @@ export class NewsComponent implements OnInit {
   articles?: Articles[];
   authors?: Articles[];
   searchTerm: string = '';
+  notFilteredArticles?: Articles[];
   constructor(
     private readonly newsService: NewsService,
     private router: Router
@@ -20,6 +21,7 @@ export class NewsComponent implements OnInit {
     this.newsService.getNews().subscribe(
       (response) => {
         this.articles = response.articles.splice(0, 10);
+        this.notFilteredArticles = response.articles.splice(0, 10);
         this.authors = response.articles;
       },
       (error) => {
@@ -37,14 +39,13 @@ export class NewsComponent implements OnInit {
   }
 
   getSearchedNews() {
+    if (this.searchTerm?.length === 0) {
+      this.articles = this.notFilteredArticles;
+      return;
+    }
     const searchedArticle = this.articles?.filter((article) =>
       article.title.includes(this.searchTerm)
     );
-    console.log(searchedArticle);
-    if (searchedArticle && searchedArticle.length === 0) {
-      this.router.navigate(['/']);
-      return;
-    }
     this.articles = searchedArticle;
   }
 }
