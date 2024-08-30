8a64bfeee4ca7610adc98acc095abc5180077c96
Kingcedru <byishimocedrick@gmail.com>
2024-08-30 09:59:19 +0200

    Setting environment variables

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:
-    'https://news-proxy.netlify.app/?apiKey=ce0791cf7cb34350929466338dfff3a9',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl:
-    'https://newsapi.org/v2/everything?q=tesla&from=2024-07-29&sortBy=publishedAt&apiKey=ce0791cf7cb34350929466338dfff3a9',
+    'https://news-proxy.netlify.app/?apiKey=ce0791cf7cb34350929466338dfff3a9',
 };
