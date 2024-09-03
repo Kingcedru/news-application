89f88584b5fdb29c3e891b437517ebd89b292eac
Kingcedru <byishimocedrick@gmail.com>
2024-09-03 14:41:36 +0200

    changed the url

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -6,24 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class NewsService {
-  now = new Date();
-
-  year = this.now.getFullYear();
-  month = this.now.getMonth() + 1;
-  day = this.now.getDate();
-  date = `${this.year}-${this.month}-${this.day}`;
   private url =
-    'https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=ce0791cf7cb34350929466338dfff3a9';
-
-  constructor(private http: HttpClient) {
-    this.url =
-      'https://newsapi.org/v2/everything?q=tesla&from=`${this.date}`&sortBy=publishedAt&apiKey=ce0791cf7cb34350929466338dfff3a9';
-
-  }
+    'https://newsapi.org/v2/everything?q=tesla&from=2024-08-03&sortBy=publishedAt&apiKey=ce0791cf7cb34350929466338dfff3a9';
+  constructor(private http: HttpClient) {}
 
   getNews(): Observable<any> {
     return this.http.get(this.url);
   }
-
-  
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://news-proxy.netlify.app/?apiKey=ce0791cf7cb34350929466338dfff3a9',
+};
